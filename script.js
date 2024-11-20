794c248d208134dcce8922e59854a951ac673e40
Vanshita1725 <144545424+Vanshita1725@users.noreply.github.com>
2024-11-20 11:04:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript for page load animation (simple alert example)
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        alert('Welcome to Vanshita Rathod\'s Portfolio!');
+    }, 1000);
+});
