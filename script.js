26a03a6dbf9755019d3893e68697a0d55cfaf953
Vanshita1725 <144545424+Vanshita1725@users.noreply.github.com>
2024-11-22 03:24:08 +0000

    Update and rename script.js to competencies.html

--- script.js
+++
@@ -1,6 +0,0 @@
-// JavaScript for page load animation (simple alert example)
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        alert('Welcome to Vanshita Rathod\'s Portfolio!');
-    }, 1000);
-});
